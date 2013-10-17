07cfe721dd68959f1314fdb7bc88fc7af1da921f
Kaushal Keraminiyage <admin@confmgt.com>
2013-10-17 22:43:16 +0100

    Initial Commit for the OER component site files

---
+++ helper.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @version		1.5.0. OER $
+ * @package		oer
+ * @copyright	Copyright © 2013 - All rights reserved.
+ * @license		GNU/GPL
+ * @author		Dr Kaushal Keraminiyage
+ * @author mail	admin@confmgt.com
+ * @website		www.confmgt.com
+ *
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+?>
